e8380b4a00b31c1d2738b11308df4b513ee3d5e3
Aubrey Ruiz <aubreyannaruiz@gmail.com>
2023-05-22 19:22:46 -0400

    Add new stuff

---
+++ src/test/java/api/runners/TestRunner.java
@@ -0,0 +1,12 @@
+package api.runners;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunner {
+
+	@Karate.Test
+	public Karate runTests() {
+		return Karate.run("classpath:features")
+				.tags("smoke");
+	}
+}
